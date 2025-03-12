f36f2b0ed7e085c90b1ed2b78f707af0f4934681
Ryan <ryannolanco@gmail.com>
2025-03-11 20:43:00 -0600

    import styling

--- src/components/file-drop/FileDrop.jsx
+++ src/components/file-drop/FileDrop.jsx
@@ -5,15 +5,17 @@ import "./file-drop.css";
 import NavBar from "../navigation/NavBar";
 
 const FileDrop = () => {
-  const [tradingFile, setTradingFile] = useState()
+  const [tradingFile, setTradingFile] = useState();
 
-  
   const parseCSV = (csvString) => {
-    const lines = csvString.trim().split("\n").filter(line => line.trim() !== ""); // Split CSV into lines
-    const headers = headerLine.split(",").map(header => header.trim()); // Extract headers
+    const lines = csvString
+      .trim()
+      .split("\n")
+      .filter((line) => line.trim() !== ""); // Split CSV into lines
+    const headers = headerLine.split(",").map((header) => header.trim()); // Extract headers
 
-    return lines.map(line => {
-      const values = line.split(",").map(value => value.trim());
+    return lines.map((line) => {
+      const values = line.split(",").map((value) => value.trim());
       return headers.reduce((acc, header, index) => {
         acc[header] = values[index]; // Create object with key-value pairs
         return acc;
@@ -33,31 +35,32 @@ const FileDrop = () => {
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        const text = reader.result
+        const text = reader.result;
         const parsedData = parseCSV(text);
-        parsedData.forEach(item => console.log(item.AMOUNT))
-        setTradingFile(parsedData)
-
+        parsedData.forEach((item) => console.log(item.AMOUNT));
+        setTradingFile(parsedData);
       };
       reader.readAsText(file);
       console.log(file);
     });
   }, []);
 
-
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <>
-    <NavBar />
-    <div className="drop-zone-container">
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p className="drop-zone">
-          Drag 'n' drop some files here, or click to select files
-        </p>
+      <NavBar />
+      <div className="file-drop-body">
+        <div className="drop-zone-container">
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p className="drop-zone">
+              Drag 'n' drop some files
+              <br /> here, or click to select files
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };
